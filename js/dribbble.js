3587acd6f971611320d4e6dced93faeab4062266
coreyminess <fiveandpine@gmail.com>
2017-08-09 12:39:39 -0700

    testing cdn links - js / changing dribbble js to include best practice
    alt tags, rel noopener

--- js/dribbble.js
+++ js/dribbble.js
@@ -4,7 +4,7 @@
 
         shots.forEach(function(shot) {
           html.push('<div class="fl-l w-100 w-80-m w-third-l tc-m center-m pa2-l pv3 ph2 mb2"><div class="br2 nav-shadow-2 flex grow">');
-          html.push('<a href="' + shot.html_url + '" target="_blank">');
+          html.push('<a href="' + shot.html_url + '" target="_blank" rel="noopener" alt="corey mines - latest dribbble shots">');
           html.push('<img class="db br2" src="' + shot.images.hidpi + '">');
           html.push('</a></div></div>');
         });
