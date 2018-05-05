ea0c2ea741e8b95e75f41e49cbef9c7d43f99c90
coreyminess <fiveandpine@gmail.com>
2018-05-05 13:03:36 -0700

    Remove Unused JS

--- js/jribbble6.js
+++ js/jribbble6.js
@@ -19,16 +19,3 @@ $.ajax({
             }
         }
     });
-
-// $.jribbble.users('coreymines').shots({'per_page': 3}).then(function(shots) {
-//   var html = [];
-
-//   shots.forEach(function(shot) {
-//     html.push('<div class="fl-l w-100 w-80-m w-third-l tc-m center-m pa2-l pv3 ph2 mb2"><div class="br2 nav-shadow-2 flex grow">');
-//     html.push('<a href="' + shot.html_url + '" target="_blank" rel="noopener">');
-//     html.push('<img class="db br2" alt="corey mines - latest dribbble shots" src="' + shot.images.hidpi + '">');
-//     html.push('</a></div></div>');
-//   });
-
-//   $('.dribbble-shots').html(html.join(''));
-// });
