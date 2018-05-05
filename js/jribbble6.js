89bc993aa91a2ca5ce6f71992bf4207e2ca17b33
coreyminess <fiveandpine@gmail.com>
2018-05-05 12:32:02 -0700

    jribbble auth test v1

---
+++ js/jribbble6.js
@@ -0,0 +1,42 @@
+// Set the Access Token
+var accessToken = 
+'43595dd2dbf04bd0ddfa7515e6c1fe6f4d0866660a00775c261c94f665f8a2e2';
+
+// Call Dribbble v2 API
+// $.ajax({
+//     url: 'https://api.dribbble.com/v2/user/shots?access_token='+43595dd2dbf04bd0ddfa7515e6c1fe6f4d0866660a00775c261c94f665f8a2e2,
+//     dataType: 'json',
+//     type: 'GET',
+//     success: function(data) {
+//         if (data.length > 0) {
+//             $.each(data.reverse(), function(i, val) {
+//                 $('#shots').prepend(
+//                     ''
+//                 )
+//             }).users('coreymines').shots({'per_page': 3}).then(function(shots) {
+//   var html = [];
+
+//   shots.forEach(function(shot) {
+//     html.push('<div class="fl-l w-100 w-80-m w-third-l tc-m center-m pa2-l pv3 ph2 mb2"><div class="br2 nav-shadow-2 flex grow">');
+//     html.push('<a href="' + shot.html_url + '" target="_blank" rel="noopener">');
+//     html.push('<img class="db br2" alt="corey mines - latest dribbble shots" src="' + shot.images.hidpi + '">');
+//     html.push('</a></div></div>');
+//   });
+
+//   $('.dribbble-shots').html(html.join(''));
+//     }
+
+// });
+
+$.jribbble.users('coreymines').shots({'per_page': 3}).then(function(shots) {
+  var html = [];
+
+  shots.forEach(function(shot) {
+    html.push('<div class="fl-l w-100 w-80-m w-third-l tc-m center-m pa2-l pv3 ph2 mb2"><div class="br2 nav-shadow-2 flex grow">');
+    html.push('<a href="' + shot.html_url + '" target="_blank" rel="noopener">');
+    html.push('<img class="db br2" alt="corey mines - latest dribbble shots" src="' + shot.images.hidpi + '">');
+    html.push('</a></div></div>');
+  });
+
+  $('.dribbble-shots').html(html.join(''));
+});
