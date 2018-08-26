3244eaa74a2ad4f4942b33130c236c57ea27a5f8
coreyminess <fiveandpine@gmail.com>
2018-08-25 19:06:08 -0700

    test 5

--- js/pageanimations.js
+++ js/pageanimations.js
@@ -1,4 +1,5 @@
 
 window.sr = ScrollReveal({ reset: true });
+sr.reveal('.reveal1', { container: 'section' });
 
-sr.reveal('.center', {origin: 'right', distance: '300px', duration: 2000});
+sr.reveal('.reveal1', {origin: 'right', distance: '300px', duration: 2000});
