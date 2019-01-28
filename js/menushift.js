467efac8bd97c208c7fceab7c17a3a46748e0455
coreyminess <fiveandpine@gmail.com>
2019-01-27 20:55:03 -0700

    Deploying website 2.0
    All new.

--- js/menushift.js
+++ js/menushift.js
@@ -1,16 +1,1 @@
-$(document).ready(function(){
-            var scroll_pos = 0;
-            $(document).scroll(function() {
-                scroll_pos = $(this).scrollTop();
-                if(scroll_pos = 0) {
-                    $("#nav-background").css('#nav-background','white');
-                    $("#nav-links").css('background', 'none');
-                    $("bar-change").css('background', 'white');
-                }
-                else {
-                    $("#nav-background").css('#nav-background','black');
-                    $("#nav-links").css('background', 'white');
-                    $("body input + label span").css('background', 'black');
-                }
-            });
-        });
+$(document).ready(function(){$(document).scroll(function(){$(this).scrollTop(),0?($("#nav-background").css("#nav-background","white"),$("#nav-links").css("background","none"),$("bar-change").css("background","white")):($("#nav-background").css("#nav-background","black"),$("#nav-links").css("background","white"),$("body input + label span").css("background","black"))})});
